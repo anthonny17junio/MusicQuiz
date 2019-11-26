baa5dc9e76a2765dbbb195293f226a9dcd4d91e9
Anthonny Pazmiño <anthonny17junio@gmail.com>
2019-11-26 10:56:53 +0100

    Prueba Botón

--- app/src/main/java/es/usj/mastersa/apazmino/musicquiz/MainActivity.kt
+++ app/src/main/java/es/usj/mastersa/apazmino/musicquiz/MainActivity.kt
@@ -2,8 +2,10 @@ package es.usj.mastersa.apazmino.musicquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btn:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
